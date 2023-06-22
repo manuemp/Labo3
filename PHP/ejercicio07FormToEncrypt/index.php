aedf2b4cdd8ae4a080cbbdc5ba178e6f324ef546
Manuel Pedro <manuelpedro@MacBook-Air-de-Manuel.local>
2023-06-22 16:46:13 -0300

    configuracion nginx

---
+++ PHP/ejercicio07FormToEncrypt/index.php
@@ -0,0 +1,32 @@
+    <?php
+        if(isset($_POST["encriptacion"]))
+        {
+            $claveEncriptadamd5 = md5($_POST["encriptacion"]);
+            $claveEncriptadaSha = sha1($_POST["encriptacion"]);
+            echo "Clave sin encriptar: " . $_POST["encriptacion"] . "<br>";
+            echo "Clave encriptada en md5 (128 bits o 16 pares hexadecimales): <br> $claveEncriptadamd5 <br>";
+            echo "Clave encriptada en sha1 (160 bits o 20 pares hexadecimales): <br> $claveEncriptadaSha <br>";
+        }
+        else
+        {
+    ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Encriptado</title>
+</head>
+<body>
+    <form action="./index.php" method="post">
+        <label for="encriptacion">Ingrese texto a encriptar: </label>
+        <input type="text" id="encriptacion" name="encriptacion"><br>
+        <input type="submit" value="Encriptar">
+    </form>
+
+</body>
+</html>
+
+    <?php
+        }
+    ?>
